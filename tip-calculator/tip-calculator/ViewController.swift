f7fbbdb2de5974a5c25288894553601ad0ea0722
Hoon94 <dleognsdl1@naver.com>
2024-06-07 21:39:41 +0900

    feat: Initial commit for project

---
+++ tip-calculator/tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Daehoon Lee on 6/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
